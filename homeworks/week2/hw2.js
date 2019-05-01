fb3326a3c8266af231216edd139b827b759d9b9a
yoding <email.yuzhen@gmail.com>
2019-05-01 20:55:38 +0800

    add all week2 hw, but have a question at hw3

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,11 @@
 function capitalize(str) {
-  return str;
+  const Upp = str.slice(0, 1);
+  return Upp.toUpperCase() + str.slice(1);
 }
 
 console.log(capitalize('hello'));
+console.log(capitalize('nick'));
+console.log(capitalize('Nick'));
+console.log(capitalize('#Nick'));
+console.log(capitalize('#123Nick'));
+console.log(capitalize(''));
--- homeworks/week2/hw4.js
+++ homeworks/week2/hw4.js
@@ -1,4 +1,11 @@
 function printFactor(n) {
+  let i = 0;
+  while (i <= n) {
+    if (n % i === 0) {
+      console.log(i);
+    }
+    i += 1;
+  }
   return n;
 }
 
